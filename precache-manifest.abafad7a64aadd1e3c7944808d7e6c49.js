11d6024fee8300ec05c680eb6d38f9b395458e8f
jjswift <supercheater2018@yandex.ru>
2020-10-05 01:07:59 +0300

    Updates

---
+++ precache-manifest.abafad7a64aadd1e3c7944808d7e6c49.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35776995cb691940acb82e94d9783531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9889c731ea53cd722ad3",
+    "url": "/static/css/main.bbb59593.chunk.css"
+  },
+  {
+    "revision": "15ad4c49f2aca9f773f2",
+    "url": "/static/js/2.b222739e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b222739e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9889c731ea53cd722ad3",
+    "url": "/static/js/main.1dbf93c6.chunk.js"
+  },
+  {
+    "revision": "b665535ae1f145fe0e6f",
+    "url": "/static/js/runtime-main.5ce107e7.js"
+  },
+  {
+    "revision": "267111155a54d4b19cccd1f6cf86cf46",
+    "url": "/static/media/5opka.26711115.png"
+  },
+  {
+    "revision": "7aff23368d947ea6722a9aee992a2c19",
+    "url": "/static/media/BeHealthy.7aff2336.png"
+  },
+  {
+    "revision": "bad7af6b6ac6b1dd896879e86eaeddb0",
+    "url": "/static/media/BookingCar.bad7af6b.png"
+  },
+  {
+    "revision": "55e7680a2fbb46b1f7b1f0dd5c15a62e",
+    "url": "/static/media/BurgerKing.55e7680a.png"
+  },
+  {
+    "revision": "7f6253ac90b09e3421217f9d13d425a5",
+    "url": "/static/media/BurgerKing.7f6253ac.png"
+  },
+  {
+    "revision": "315246ad896ca317cb30a4ae0527936c",
+    "url": "/static/media/DishCentre.315246ad.png"
+  },
+  {
+    "revision": "1e6ae1eead98df0dadce3c7f6b10aefc",
+    "url": "/static/media/Geometria-Bold.1e6ae1ee.ttf"
+  },
+  {
+    "revision": "86b1b85bdce1520f605f5ebe8ba1000c",
+    "url": "/static/media/Geometria-Medium.86b1b85b.ttf"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/static/media/Geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "649538b9bf51d60826e207ff9d4c06f6",
+    "url": "/static/media/IVI.649538b9.png"
+  },
+  {
+    "revision": "37bf00fe7e07c1b5586a473b638ae79e",
+    "url": "/static/media/Lamoda.37bf00fe.png"
+  },
+  {
+    "revision": "4eb3d3b59a6e5cf7b3a27a8af542ca79",
+    "url": "/static/media/LitRes.4eb3d3b5.png"
+  },
+  {
+    "revision": "7efd127c2920ab592b858a9febf93d7b",
+    "url": "/static/media/Megafon.7efd127c.png"
+  },
+  {
+    "revision": "00c34f2d49f8f2ae2600d68adafd76e6",
+    "url": "/static/media/Mvideo.00c34f2d.png"
+  },
+  {
+    "revision": "1b6e3e057d8bee0dbc868eb5f1414756",
+    "url": "/static/media/Mvideo50percent.1b6e3e05.png"
+  },
+  {
+    "revision": "aca250ecedeab57270d3c2493a55ba47",
+    "url": "/static/media/MyToys.aca250ec.png"
+  },
+  {
+    "revision": "7fc291ac32c0655e3283f7267480ce64",
+    "url": "/static/media/OZON.7fc291ac.png"
+  },
+  {
+    "revision": "28eed9c93496dd4375cbc4c1ea47187b",
+    "url": "/static/media/Respublic.28eed9c9.png"
+  },
+  {
+    "revision": "4f9d208631c203445461f807b72cbf5c",
+    "url": "/static/media/SberInsurance.4f9d2086.png"
+  },
+  {
+    "revision": "920a91936c9c7957d3e55fe1b22331e0",
+    "url": "/static/media/TezTour.920a9193.png"
+  },
+  {
+    "revision": "c0bd3e8dce96d84dd750d2ba9da6b3e2",
+    "url": "/static/media/Tripster.c0bd3e8d.png"
+  },
+  {
+    "revision": "9eff5fc75e81abc3e939e3405b72d22d",
+    "url": "/static/media/app-store.9eff5fc7.svg"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "/static/media/burger.a450cc10.svg"
+  },
+  {
+    "revision": "3bbe74fbc7285ce49c9825ec683c18fa",
+    "url": "/static/media/close_icon.3bbe74fb.svg"
+  },
+  {
+    "revision": "d5cd9b785763b751d3d1ad5526485592",
+    "url": "/static/media/euronetBlackFriday.d5cd9b78.png"
+  },
+  {
+    "revision": "791fb0d279cadf58dc3ef99ea98c4a01",
+    "url": "/static/media/fb.791fb0d2.svg"
+  },
+  {
+    "revision": "2c9b87057a11e95c6ab428a29080b4c1",
+    "url": "/static/media/google-play.2c9b8705.svg"
+  },
+  {
+    "revision": "cd7ef97a963b1609b505eba3e793895e",
+    "url": "/static/media/inst.cd7ef97a.svg"
+  },
+  {
+    "revision": "936938ca5d3a1e3fe2bf885d5c38124a",
+    "url": "/static/media/ok.936938ca.svg"
+  },
+  {
+    "revision": "382c3d461b88fc6e65395982e59d4571",
+    "url": "/static/media/sberlogo.382c3d46.svg"
+  },
+  {
+    "revision": "7e497b96075e5d446fa25eb072fab210",
+    "url": "/static/media/search_icon.7e497b96.svg"
+  },
+  {
+    "revision": "d7914f36dc44061a31c92cec78f23308",
+    "url": "/static/media/spasibo_logo.d7914f36.png"
+  },
+  {
+    "revision": "8ea1ca899588a178df885d446327a9a8",
+    "url": "/static/media/technoparkTech.8ea1ca89.png"
+  },
+  {
+    "revision": "ca8724be606b095714d0a4f6b1f1602e",
+    "url": "/static/media/vk.ca8724be.svg"
+  }
+]);
